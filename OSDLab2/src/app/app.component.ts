0b2d90caf7fa93b30102fedc18158bd18596ed74
roisin-m <s00223865@atu.ie>
2025-02-05 15:22:44 +0000

    bootstrap and app component html

--- OSDLab2/src/app/app.component.ts
+++ OSDLab2/src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 //signal service import
 import { SignalService } from './services/signal/signal.service';
+import { Child1Component } from "./components/child1/child1/child1.component";
+import { Child2Component } from "./components/child2/child2.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Child1Component, Child2Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
